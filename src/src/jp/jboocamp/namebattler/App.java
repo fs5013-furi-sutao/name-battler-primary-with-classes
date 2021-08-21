f3fc94cae85ac131cf229bbf3bb100406d863261
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-08-21 21:10:12 +0900

    Config クラスを作成

--- src/src/jp/jboocamp/namebattler/App.java
+++ src/src/jp/jboocamp/namebattler/App.java
@@ -1,7 +1,9 @@
 package src.jp.jboocamp.namebattler;
 
+import src.jp.jboocamp.setting.Config;
+
 public class App {
-    private static final int NUM_OF_PLAYERS = 3;
+    private static final int NUM_OF_PLAYERS = Config.Values.NUM_OF_PLAYERS;
 
     public static void main(String[] args) {
         BattleField field = new BattleField(NUM_OF_PLAYERS);
---
+++ src/src/jp/jboocamp/setting/Config.java
@@ -0,0 +1,7 @@
+package src.jp.jboocamp.setting;
+
+public class Config {
+    public class Values {
+        public static final int NUM_OF_PLAYERS = 4;
+    }
+}
